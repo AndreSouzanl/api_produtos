a7b69d6943886c47331e901350c508c789050acc
Andresouzanl <devals2022@gmail.com>
2026-02-21 20:27:29 -0300

    atualiandoReset

--- src/servicos/servico_email.js
+++ src/servicos/servico_email.js
@@ -7,9 +7,7 @@ const EMAIL_PASS = "josq nwil ohbf dosy"; // senha de app gerada
 
 // Criando o transporte do Nodemailer
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true, // SSL
+  service: "gmail",
   auth: {
     user: EMAIL_USER,
     pass: EMAIL_PASS,
@@ -18,20 +16,25 @@ const transporter = nodemailer.createTransport({
 
 // Função para enviar email de reset
 export async function enviarEmailReset(destinatario, token) {
-  const resetUrl = `https://lista-compras-frontend-cu31.vercel.app/reset-password?token=${token}`;
+  try {
+    const resetUrl = `https://lista-compras-frontend-cu31.vercel.app/reset-password?token=${token}`;
 
-  const mailOptions = {
-    from: EMAIL_USER,
-    to: destinatario, // aqui é dinâmico!
-    subject: "Redefinição de senha",
-    html: `
-      <p>Você solicitou a redefinição de senha.</p>
+    const mailOptions = {
+      from: EMAIL_USER,
+      to: destinatario,
+      subject: "Redefinição de senha",
+      html: `
+        <p>Você solicitou a redefinição de senha.</p>
       <p>Clique no link abaixo para criar uma nova senha:</p>
       <a href="${resetUrl}">Redefinir senha</a>
       <p>O link expira em 1 hora.</p>
-    `,
-  };
+      `,
+    };
 
-  await transporter.sendMail(mailOptions);
+    const info = await transporter.sendMail(mailOptions);
+
+    console.log("✅ Email enviado:", info.response);
+  } catch (error) {
+    console.error("❌ Erro ao enviar email:", error);
+  }
 }
-
